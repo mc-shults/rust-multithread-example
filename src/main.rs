7905500b73d8ca7aa9fe9d8950f976f34f2e8270
mc-shults <dmitrii-1997@outlook.com>
2018-08-02 17:12:52 +0000

    test methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::thread;
+
+trait Worker {
+    fn execute(&self);
+}
+
+struct Produser {}
+
+impl Worker for Produser {
+    fn execute(&self) {
+        println!("Produse!");
+    }
+}
+
+fn run<T>(obj: T)
+// -> thread::JoinHandle<()>
+where
+    T: Worker + Send,
+{
+    obj.execute();
+    //thread::spawn(move || {
+    //    obj.execute();
+    //})
+}
+
+fn say_hello() {
+    println!("Hello, world!")
+}
+
 fn main() {
+    say_hello();
     println!("Hello, world!");
+    let child = thread::spawn(move || {
+        println!("New thread :)");
+    });
+    child.join().unwrap();
+    let p = Produser {};
+
+    let child1 = thread::spawn(move || {
+        p.execute();
+    });
+    child1.join().unwrap();
 }
